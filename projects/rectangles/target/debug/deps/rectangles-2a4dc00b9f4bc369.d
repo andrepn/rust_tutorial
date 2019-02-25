653c95f7ff4c7e6807a71c944c84ff67cad33089
Leon the gorilla <andrew_noel@live.com>
2019-02-25 16:09:40 -0600

    rectangles struct through chapter 5

---
+++ projects/rectangles/target/debug/deps/rectangles-2a4dc00b9f4bc369.d
@@ -0,0 +1,5 @@
+/home/leon/rust/rust_tutorial/projects/rectangles/target/debug/deps/rectangles-2a4dc00b9f4bc369: src/main.rs
+
+/home/leon/rust/rust_tutorial/projects/rectangles/target/debug/deps/rectangles-2a4dc00b9f4bc369.d: src/main.rs
+
+src/main.rs:
---
+++ projects/rectangles/target/debug/rectangles.d
@@ -0,0 +1,1 @@
+/home/leon/rust/rust_tutorial/projects/rectangles/target/debug/rectangles: /home/leon/rust/rust_tutorial/projects/rectangles/src/main.rs
